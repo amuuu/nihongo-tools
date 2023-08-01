aa2de922f5e0f2e4f286e59f2dbe66c9fb2af843
amu <aamouie01@gmail.com>
2023-08-01 04:29:59 +0330

    add files

---
+++ verbforms_data.js
@@ -0,0 +1,22 @@
+const verbsDataJSON = `
+[
+  {
+    "dictionaryForm": "見る",
+    "pronunciation": "みる",
+    "masuForm": "見ます",
+    "teForm": "見て",
+    "taForm": "見た",
+    "naiForm": "見ない",
+    "meaning": "to see, to look at"
+  },
+  {
+    "dictionaryForm": "食べる",
+    "pronunciation": "たべる",
+    "masuForm": "食べます",
+    "teForm": "食べて",
+    "taForm": "食べた",
+    "naiForm": "食べない",
+    "meaning": "to eat"
+  }
+]
+`;
---
+++ verbforms_script.js
@@ -0,0 +1,107 @@
+const targetVerbElement = document.getElementById("targetVerb");
+const targetFormElement = document.getElementById("targetForm");
+const answerInputElement = document.getElementById("answer");
+const resultMessageElement = document.getElementById("resultMessage");
+const otherForm1Element = document.getElementById("otherForm1");
+const otherForm2Element = document.getElementById("otherForm2");
+const otherForm3Element = document.getElementById("otherForm3");
+const otherForm4Element = document.getElementById("otherForm4");
+const verbMeaningElement = document.getElementById("verbMeaning");
+const sessionStatsElement = document.getElementById("sessionStats");
+
+let currentVerbIndex = 0;
+let correctAnswers = 0;
+let totalAttempts = 0;
+
+const verbsData = JSON.parse(verbsDataJSON);
+let cachedVerbData = null;
+
+function displayRandomVerb() {
+  if (verbsData.length === 0) {
+    return;
+  }
+
+  const randomVerb = verbsData[Math.floor(Math.random() * verbsData.length)];
+    cachedVerbData = randomVerb;
+
+  const forms = ["Vて", "Vます", "Vた", "Vない"];
+  const randomForm = forms[Math.floor(Math.random() * forms.length)];
+
+  targetVerbElement.textContent = `${randomVerb.dictionaryForm} (${randomVerb.pronunciation})`;
+  targetFormElement.textContent = randomForm;
+
+  answerInputElement.value = "";
+  resultMessageElement.textContent = "";
+  otherForm1Element.textContent = "";
+  otherForm2Element.textContent = "";
+  otherForm3Element.textContent = "";
+  otherForm4Element.textContent = "";
+  verbMeaningElement.textContent = "";
+}
+
+function checkAnswer(event) {
+  event.preventDefault(); 
+
+  resultMessageElement.textContent = "";
+
+  const userAnswer = answerInputElement.value.trim();
+  const correctAnswer = getCorrectAnswer();
+
+  const isCorrect = userAnswer === correctAnswer;
+
+  if (isCorrect) {
+    resultMessageElement.textContent = "おめでとう！🎊";
+    correctAnswers++;
+  } else {
+    resultMessageElement.textContent = `😔➡️ ${correctAnswer}`;
+  }
+
+  totalAttempts++;
+  sessionStatsElement.textContent = `✅ ${correctAnswers}/${totalAttempts}`;
+
+  if (cachedVerbData) {
+      otherForm1Element.textContent = `Vます: ${cachedVerbData.masuForm}`;
+      otherForm2Element.textContent = `Vて: ${cachedVerbData.teForm}`;
+      otherForm3Element.textContent = `Vた: ${cachedVerbData.taForm}`;
+      otherForm4Element.textContent = `Vない: ${cachedVerbData.naiForm}`;
+      verbMeaningElement.textContent = `Meaning: ${cachedVerbData.meaning}`;
+    }
+
+  if (isCorrect) {
+    setTimeout(displayRandomVerb, 2000);
+  }
+  else
+  {
+    setTimeout(displayRandomVerb, 7000);
+  }
+}
+
+function getCorrectAnswer() {
+  const displayedVerb = targetVerbElement.textContent.split(" ")[0]; 
+  const displayedForm = targetFormElement.textContent;
+  
+  const verb = verbsData.find(v => v.dictionaryForm === displayedVerb);
+  
+  switch (displayedForm) {
+    case "Vて":
+      return verb.teForm;
+    case "Vます":
+      return verb.masuForm;
+    case "Vた":
+      return verb.taForm;
+    case "Vない":
+      return verb.naiForm;
+    default:
+      return "";
+  }
+}
+const nextVerbButton = document.getElementById("nextVerbButton");
+
+function handleNextVerbButtonClick() {
+  displayRandomVerb();
+}
+
+document.getElementById("answerForm").addEventListener("submit", checkAnswer);
+nextVerbButton.addEventListener("click", handleNextVerbButtonClick);
+
+//displayRandomVerb();
